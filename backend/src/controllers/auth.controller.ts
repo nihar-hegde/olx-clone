585571e4e2327a69db5b36e0cf380abd53bb7057
nihar-hegde <niharhegde163@gmail.com>
2024-08-21 07:32:59 +0530

    httpOnly to false

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -98,7 +98,6 @@ export const login = async (req: Request, res: Response) => {
     });
 
     res.cookie("token", token, {
-      httpOnly: true,
       secure: process.env.NODE_ENV === "production",
       sameSite: "lax",
       maxAge: 3600000,
